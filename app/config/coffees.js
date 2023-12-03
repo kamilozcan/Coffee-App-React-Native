5e4bfe8d276a8c834c9f177883a65b9aa87792bc
Kamil Ozcan <kamilozcann@gmail.com>
2023-12-03 18:19:52 -0500

    Modified coffees and categories

---
+++ app/config/coffees.js
@@ -0,0 +1,13 @@
+//list of coffees
+export default [
+    {
+        id: 1,
+        name: "Cappuccino",
+        image: require("../../assets/coffees/nathan-dumlao-c2Y16tC3yO8-unsplash.jpg"),
+        price: "2.00",
+        description: "Cappuccino is a coffee drink made with espresso and hot milk. It is traditionally prepared with steamed milk, and is traditionally topped with a small amount of foam.",
+        categoryId: 1,
+        rating: 4.5,
+        included: "With Oat milk",
+    },
+]
